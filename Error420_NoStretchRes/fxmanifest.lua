1dcbb0e655d72bec88802698124dbcedfc5d0675
Error420 <insomniaroleplay2024@gmail.com>
2024-11-12 19:18:56 +1100

    Update

--- Error420_NoStretchRes/fxmanifest.lua
+++ Error420_NoStretchRes/fxmanifest.lua
@@ -1,8 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 author 'Error420 Developments'
 description 'FiveM Block Stretch Resolution - Error420 Developments'
+
+shared_script '@ox_lib/init.lua'
 
 client_script 'client/*.lua'
 
